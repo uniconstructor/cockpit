d2d30991d0d87adc4c51d409ae821fbb9887b85e
Smirnov Ilia <php1602agregator@gmail.com>
2015-05-30 01:06:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 defined('YII_DEBUG') or define('YII_DEBUG', isset($_SERVER['YII_DEBUG']) ? $_SERVER['YII_DEBUG'] : true);
 defined('YII_ENV') or define('YII_ENV', isset($_SERVER['YII_ENV']) ? $_SERVER['YII_ENV'] : 'dev');
+define('COCKPIT_EMBEDED', 1);
 
 require(__DIR__ . '/../../../vendor/autoload.php');
 require(__DIR__ . '/../../../vendor/yiisoft/yii2/Yii.php');
